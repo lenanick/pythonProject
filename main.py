5502030b5cf9c009b9168783023b8b3ffad4ce84
Пользователь <ltsiuper@gmail.com>
2022-12-04 22:59:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# print("Hello future QA Automation")
+
+item = 1
+name = 'Lena'
+result = str(item)+ ' ' + name
+print (result)
+
+items = [item, name, 'Illia', 33, [11,22,"hello"], {'age':30}, True, False, (0,1,3), 4.5]
+
+print(len(items))
