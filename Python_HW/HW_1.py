58d5a619dac29fb8a5e979b81ea15e3ba6b849f1
lenanick <ltsiuper@gmail.com>
2022-12-05 14:28:49 +0200

    lesson_1

---
+++ Python_HW/HW_1.py
@@ -0,0 +1,15 @@
+# 1. Создать переменную типа String
+item_1 = 'Lena'
+
+# 2. Создать переменную типа Integer
+item_2 = 25
+
+# 3. Создать переменную типа Float
+item_3 = 2.5
+print(type(item_3))
+
+# 4 Создать переменную типа Bytes
+item_4 = b"Hello"
+print(type(item_4))
+
+# 5 Создать переменную типа List
--- main.py
+++ main.py
@@ -4,8 +4,10 @@
 item = 1
 name = 'Lena'
 result = str(item)+ ' ' + name
-print (result)
+print(result)
 
 items = [item, name, 'Illia', 33, [11,22,"hello"], {'age':30}, True, False, (0,1,3), 4.5]
 
 print(len(items))
+
+print(items[4][2])
