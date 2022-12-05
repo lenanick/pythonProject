5556c0aedbf6207f4a266288867a5ec58c9b6455
lenanick <ltsiuper@gmail.com>
2022-12-05 14:53:15 +0200

    lesson_1

--- Python_HW/HW_1.py
+++ Python_HW/HW_1.py
@@ -13,3 +13,21 @@ item_4 = b"Hello"
 print(type(item_4))
 
 # 5 Создать переменную типа List
+item_5 = ['Qa', 'Developer', 'PM']
+print(type(item_5))
+
+# 6 Создать переменную типа Tuple
+item_6 = ('Elena', 25, 'Illia')
+print(type(item_6))
+
+# 7 Создать переменную типа Set
+item_7 = {'QA', 1, 2}
+print(type(item_7))
+
+# 8 Создать переменную типа Frozen set
+item_8 = frozenset('QA Manual')
+print(type(item_8))
+
+# 9 Создать переменную типа Dict
+item_9 = {'age': 38}
+print(type(item_9))
