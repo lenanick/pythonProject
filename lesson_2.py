9214678842bc5f6af908c359cf84ebac0e5b1ff2
lenanick <ltsiuper@gmail.com>
2022-12-16 10:44:16 +0200

    lesson_2

--- lesson_2.py
+++ lesson_2.py
@@ -9,12 +9,31 @@ items_list_e = []
 b_item_t = True
 b_item_f = False
 
-if b_item_f:
-    print('True item')
+compare_item = item_1 > item_0
+
+if compare_item:
+    print('-- 1 -- True item')
+
+    new_name = 'Olga'
+    user_age = 30
+    print('user_age', user_age)
+
+    if new_name:
+
+        new_user_age = user_age - 5
+        print('new_user_age', new_user_age)
+
+
+elif name:
+    print('-- 2 -- Name item')
+elif items_list:
+    print('-- 3 -- items-list')
+elif 10 > 20:
+    print('-- 4 -- items-list')
 else:
     print('False item')
 
-if b_item_f:
-    print('----2 True item')
+
+
 
 
