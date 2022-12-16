5772171051653604171616767719c80e5070fa40
lenanick <ltsiuper@gmail.com>
2022-12-16 10:27:33 +0200

    lesson_2

---
+++ lesson_2.py
@@ -0,0 +1,20 @@
+item_1 = 1
+item_0 = 0
+
+name = 'Lena'
+items_list = ['11', 22]
+items_list_e = []
+
+
+b_item_t = True
+b_item_f = False
+
+if b_item_f:
+    print('True item')
+else:
+    print('False item')
+
+if b_item_f:
+    print('----2 True item')
+
+
